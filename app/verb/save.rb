8fd5e1acfdb95148ce29921d8b114f36969e8cd3
moveson <mark.oveson@gmail.com>
2022-10-18 22:32:13 -0600

    Save UI

---
+++ app/verb/save.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "./app/persist"
+
+module Verb
+  class Save < ::BaseVerb
+    # @return [String (frozen)]
+    def execute
+      if noun.nil? || noun == "game"
+        print "Save the game under what filename? "
+        response = gets.chomp
+        if response.present?
+          if ::Persist.save(state, response)
+            "Game saved under filename #{response}."
+          else
+            "I couldn't save the game, sorry."
+          end
+        else
+          "I can't save a game without a filename."
+        end
+      else
+        "I don't know how to save a #{noun}."
+      end
+    end
+  end
+end

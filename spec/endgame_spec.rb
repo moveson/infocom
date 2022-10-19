aaa8180102b7318ea645dfe9f6f833d7f402ee60
moveson <mark.oveson@gmail.com>
2022-10-19 00:47:23 -0600

    Fix the Endgame spec

--- spec/endgame_spec.rb
+++ spec/endgame_spec.rb
@@ -1,68 +1,50 @@
 # frozen_string_literal: true
 
 require "./app/endgame"
-require "./app/state"
+require "./app/models/item"
+require "./app/models/state"
 
 RSpec.describe ::Endgame do
-  describe "set_state" do
+  describe ".condition" do
+    let(:result) { described_class.condition(state) }
     let(:state) { ::State.new }
     let(:player_location_key) { "quiet_meadow" }
     let(:sword_location_key) { "quiet_meadow" }
 
     before do
-      state.location_key = player_location_key
-      state.items["sword"].location_key = sword_location_key
+      state.player_location_key = player_location_key
+      state.items["sword"] = ::Item.new(location_key: sword_location_key)
     end
 
     context "when no win or loss criteria are met" do
-      it "does not set won or lost to true" do
-        described_class.set_state(state)
-        expect(state.won?).to eq(false)
-        expect(state.lost?).to eq(false)
-      end
+      it { expect(result).to eq("in_progress") }
     end
 
     context "when win criteria are met" do
       let(:player_location_key) { "sunlit_hill" }
       let(:sword_location_key) { "inventory" }
 
-      it "sets won to true" do
-        described_class.set_state(state)
-        expect(state.won?).to eq(true)
-        expect(state.lost?).to eq(false)
-      end
+      it { expect(result).to eq("won") }
     end
 
     context "when location key is not met" do
       let(:player_location_key) { "quiet_meadow" }
       let(:sword_location_key) { "inventory" }
 
-      it "does not set won to true" do
-        described_class.set_state(state)
-        expect(state.won?).to eq(false)
-        expect(state.lost?).to eq(false)
-      end
+      it { expect(result).to eq("in_progress") }
     end
 
     context "when sword key is not met" do
       let(:player_location_key) { "sunlit_hill" }
       let(:sword_location_key) { "quiet_meadow" }
 
-      it "does not set won to true" do
-        described_class.set_state(state)
-        expect(state.won?).to eq(false)
-        expect(state.lost?).to eq(false)
-      end
+      it { expect(result).to eq("in_progress") }
     end
 
     context "when lost criteria are met" do
       let(:player_location_key) { "deadly_pit" }
 
-      it "sets lost to true" do
-        described_class.set_state(state)
-        expect(state.won?).to eq(false)
-        expect(state.lost?).to eq(true)
-      end
+      it { expect(result).to eq("lost") }
     end
   end
 end

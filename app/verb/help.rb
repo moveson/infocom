60d52747cab26136101eba4132477dfd87947c62
moveson <mark.oveson@gmail.com>
2022-10-21 07:33:34 -0600

    Improve the help text

--- app/verb/help.rb
+++ app/verb/help.rb
@@ -7,8 +7,11 @@ module Verb
       <<~TEXT
         The game is simple:
         I can understand simple commands in plain English, like 'go west' or 'take the key'.
-        I can also understand some more complex commands, like 'put the key in the chest'.
+        I can also understand some more complex commands, like 'put the book in the box'.
         I ignore certain words like 'a' and 'the', so feel free to use them or not, as you prefer.
+
+        Single letters are interpreted as movements in compass directions.
+        For example, 'n' means 'go north' and 'e' means 'go east'.
 
         To see what you are carrying at any time, you can type 'inventory' or 'inv'.
         To describe your location in full, type 'look'.

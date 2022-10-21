a3bb07e9e86023dd90416a6b95f30ac3455a1144
moveson <mark.oveson@gmail.com>
2022-10-21 01:52:15 -0600

    Improve help

--- app/verb/help.rb
+++ app/verb/help.rb
@@ -5,10 +5,18 @@ module Verb
     # @return [String (frozen)]
     def execute
       <<~TEXT
-        I can understand two-word commands in plain English, like 'go west' or 'take key'.
-        To see what you are carrying at any time, you can type 'inventory'.
+        The game is simple:
+        I can understand simple commands in plain English, like 'go west' or 'take the key'.
+        I can also understand some more complex commands, like 'put the key in the chest'.
+        I ignore certain words like 'a' and 'the', so feel free to use them or not, as you prefer.
+
+        To see what you are carrying at any time, you can type 'inventory' or 'inv'.
         To describe your location in full, type 'look'.
+        To save your progress, type 'save'.
+        To restore a saved game, type 'restore'.
         To exit the game, type 'quit'.
+
+        Enjoy!
       TEXT
     end
   end

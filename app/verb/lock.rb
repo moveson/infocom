e23d636c8854cbc7136ec3c1994ea5c023b2d5ad
moveson <mark.oveson@gmail.com>
2022-10-21 02:02:51 -0600

    New verb: Lock (and improve Unlock)

--- app/verb/lock.rb
+++ app/verb/lock.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 module Verb
-  class Unlock < ::BaseVerb
+  class Lock < ::BaseVerb
     # @return [String (frozen)]
     def execute
       if noun.nil?
-        "You will need to say what you want to unlock."
+        "You will need to say what you want to lock."
       elsif item.nil?
         "I don't see a #{noun} here."
       elsif item.location_key == state.player_location_key || item.location_key == "inventory"
-        if item.unlocked?
-          "The #{noun} is already unlocked."
+        if item.locked?
+          "The #{noun} is already locked."
         elsif state.inventory.any? { |item| item.can_unlock == noun }
-          item.locked = false
-          "You unlock the #{noun}."
+          item.locked = true
+          "You lock the #{noun}."
         elsif item.lockable?
-          "You don't have anything that will unlock the #{noun}."
+          "You don't have anything that will lock the #{noun}."
         else
-          "You are unable to unlock the #{noun}."
+          "You can't lock the #{noun}."
         end
       else
         "I don't see a #{noun} here."

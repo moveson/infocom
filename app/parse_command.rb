c6013df53df0ab379f95587254658a34b842dfd9
moveson <mark.oveson@gmail.com>
2022-10-21 21:36:52 -0600

    Generalize directions into implicit verbs

--- app/parse_command.rb
+++ app/parse_command.rb
@@ -11,8 +11,7 @@ class ParseCommand
   RULES_HASH = YAML.load(File.read(RULES_FILE_PATH))
   SYNONYMS = RULES_HASH["synonyms"]
   IGNORED_WORDS = RULES_HASH["ignored_words"]
-
-  DIRECTIONS = %w[north east south west]
+  IMPLICIT_VERBS = RULES_HASH["implicit_verbs"]
 
   # @param [String] command
   # @param [State] state
@@ -20,10 +19,23 @@ class ParseCommand
   def self.perform(command, _state)
     command = command.to_s.downcase
     words = command.split
-    words.reject! { |word| word.in?(IGNORED_WORDS) }
-    words.map! { |word| SYNONYMS[word].present? ? SYNONYMS[word] : word }
-    words.unshift("go") if DIRECTIONS.include?(words[0])
+    remove_ignored_words(words)
+    map_synonyms(words)
+    add_implicit_verb(words)
 
     ::Grammar.new(verb: words[0], noun: words[1], preposition: words[2], object: words[3])
+  end
+
+  private_class_method def self.remove_ignored_words(words)
+    words.reject! { |word| word.in?(IGNORED_WORDS) }
+  end
+
+  private_class_method def self.map_synonyms(words)
+    words.map! { |word| SYNONYMS[word].present? ? SYNONYMS[word] : word }
+  end
+
+  private_class_method def self.add_implicit_verb(words)
+    implicit_verb = IMPLICIT_VERBS[words.first]
+    words.unshift(implicit_verb) if implicit_verb.present?
   end
 end

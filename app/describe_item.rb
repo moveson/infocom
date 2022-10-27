c10e05f7e1b69f7be3a8c5a23b55f6708d8bf553
Mark Oveson <mark.oveson@mx.com>
2022-10-27 14:57:48 -0600

    Extract DescribeItem logic into its own class

---
+++ app/describe_item.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class DescribeItem
+  # @param [::Item] item
+  # @param [::State] state
+  # @return [String]
+  def self.perform(item, state)
+    new(item, state).perform
+  end
+
+  # @param [::Item] item
+  # @param [::State] state
+  def initialize(item, state)
+    @item = item
+    @state = state
+    @text_segments = []
+  end
+
+  # @return [String]
+  def perform
+    item_text = item.described? ? item.name.articleize : item.description
+    text_segments << "You see #{item_text}"
+    item.described = true
+
+    if item.children_visible?
+      add_text_from_contents
+    elsif item.lockable? && item.unlocked? && item.closed?
+      text_segments << "  The #{item.id} is closed"
+    end
+
+    text_segments.join("\n")
+  end
+
+  private
+
+  attr_reader :item, :state, :text_segments
+
+  def add_text_from_contents
+    contents = state.children_of_item(item)
+
+    if item.container?
+      if contents.present?
+        text_segments << "  The #{item.id} contains:"
+        contents.each do |contained_item|
+          text_segments << "    #{contained_item.name.articleize}"
+        end
+      else
+        text_segments << "  The #{item.id} is empty"
+      end
+    elsif item.surface?
+      if contents.present?
+        text_segments << "  On the #{item.id} is:"
+        contents.each do |contained_item|
+          text_segments << "    #{contained_item.name.articleize}"
+        end
+      end
+    end
+  end
+end
--- app/describe_items.rb
+++ app/describe_items.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "./app/describe_item"
+
 class DescribeItems
   # @param [::State] state
   # @return [String]
@@ -16,15 +18,7 @@ class DescribeItems
   # @return [String]
   def perform
     state.items_at_player_location.each do |item|
-      item_text = item.described? ? item.name.articleize : item.description
-      text_segments << "You see #{item_text}"
-      item.described = true
-
-      if item.children_visible?
-        add_text_from_contents(item)
-      elsif item.lockable? && item.unlocked? && item.closed?
-        text_segments << "  The #{item.id} is closed"
-      end
+      text_segments << ::DescribeItem.perform(item, state)
     end
 
     text_segments.join("\n")
@@ -33,27 +27,4 @@ class DescribeItems
   private
 
   attr_reader :state, :text_segments
-
-  # @param [Item] item
-  def add_text_from_contents(item)
-    contents = state.children_of_item(item)
-
-    if item.container?
-      if contents.present?
-        text_segments << "  The #{item.id} contains:"
-        contents.each do |contained_item|
-          text_segments << "    #{contained_item.name.articleize}"
-        end
-      else
-        text_segments << "  The #{item.id} is empty"
-      end
-    elsif item.surface?
-      if contents.present?
-        text_segments << "  On the #{item.id} is:"
-        contents.each do |contained_item|
-          text_segments << "    #{contained_item.name.articleize}"
-        end
-      end
-    end
-  end
 end

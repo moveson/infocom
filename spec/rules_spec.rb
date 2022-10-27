557dc765833d35cb095477890505bfa2c8ba58d7
Mark Oveson <mark.oveson@mx.com>
2022-10-27 14:24:12 -0600

    Update Rules spec to handle multiple adventures

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -5,32 +5,39 @@ require "./app/rules"
 # The YAML spec includes many reserved words that translate to unexpected values.
 # This spec ensures everything translates into String values.
 RSpec.describe ::Rules do
-  describe "SYNONYMS" do
-    described_class::SYNONYMS.each do |synonym, known_word|
-      context "for synonym pair #{synonym}: #{known_word}" do
-        it "creates a string pair" do
-          expect(synonym).to be_a(String)
-          expect(known_word).to be_a(String)
+  available_adventures = Dir["#{::Constants::ADVENTURES_DIRECTORY}/*"].map { |path| path.split("/").last }
+  available_adventures.each do |adventure|
+    context "for #{adventure}" do
+      rules = described_class.new(adventure)
+
+      describe "#synonyms" do
+        rules.synonyms.each do |synonym, known_word|
+          context "for synonym pair #{synonym}: #{known_word}" do
+            it "creates a string pair" do
+              expect(synonym).to be_a(String)
+              expect(known_word).to be_a(String)
+            end
+          end
         end
       end
-    end
-  end
 
-  describe "IMPLICIT_VERBS" do
-    described_class::IMPLICIT_VERBS.each do |noun, implicit_verb|
-      context "for implicit verb pair #{noun}: #{implicit_verb}" do
-        it "creates a string pair" do
-          expect(noun).to be_a(String)
-          expect(implicit_verb).to be_a(String)
+      describe "#implicit_verbs" do
+        rules.implicit_verbs.each do |noun, implicit_verb|
+          context "for implicit verb pair #{noun}: #{implicit_verb}" do
+            it "creates a string pair" do
+              expect(noun).to be_a(String)
+              expect(implicit_verb).to be_a(String)
+            end
+          end
         end
       end
-    end
-  end
 
-  describe "IGNORED_WORDS" do
-    described_class::IGNORED_WORDS.each do |word|
-      context "for #{word}" do
-        it { expect(word).to be_a(String) }
+      describe "#ignored_words" do
+        rules.ignored_words.each do |word|
+          context "for #{word}" do
+            it { expect(word).to be_a(String) }
+          end
+        end
       end
     end
   end
